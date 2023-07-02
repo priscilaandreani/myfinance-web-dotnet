cf64e5e2c0e6d34cf5e074d1e988cba2cf0ce756
priscila andreani <priscilaandreani@gmail.com>
2023-07-02 18:10:53 -0300

    feat: create account model

---
+++ myfinance-web-netcore/Models/AccountModel.cs
@@ -0,0 +1,9 @@
+namespace myfinance_web_netcore.Models;
+
+    public class AccountModel
+    {
+        public int? Id { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+    }
+
