384cab1510cddd9a8a26980f9e0801722884d80b
priscila andreani <priscilaandreani@gmail.com>
2023-07-02 16:35:34 -0300

    feat: add db context to container

--- myfinance-web-netcore/Program.cs
+++ myfinance-web-netcore/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// Add db context
+builder.Services.AddDbContext<MyFinanceDbContext>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
