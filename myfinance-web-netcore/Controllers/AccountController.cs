188e109bd0b97cb6cc33c3a27406c486dd8d443e
priscila andreani <priscilaandreani@gmail.com>
2023-07-03 20:50:18 -0300

    feat: create action to deal with edit button

--- myfinance-web-netcore/Controllers/AccountController.cs
+++ myfinance-web-netcore/Controllers/AccountController.cs
@@ -1,11 +1,14 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_netcore.Domain;
 using myfinance_web_netcore.Models;
 
 
 
 namespace myfinance_web_netcore.Controllers
 {
+    [Route("[controller]")]
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
@@ -18,6 +21,7 @@ namespace myfinance_web_netcore.Controllers
             _myFinanceDbContext = myFinanceDbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var listAccount = _myFinanceDbContext.Account;
@@ -26,22 +30,65 @@ namespace myfinance_web_netcore.Controllers
 
             foreach (var item in listAccount)
             {
-                var accountModel = new AccountModel(){
+                var accountModel = new AccountModel()
+                {
                     Id = item.Id,
                     Description = item.Description,
                     Type = item.Type,
                 };
-                listAccountModel.Add(accountModel);           
+                listAccountModel.Add(accountModel);
             }
-            
+
             ViewBag.ListAccount = listAccountModel;
             return View();
         }
 
 
-        public IActionResult Register()
+        [HttpGet]
+        [Route("Register")]
+        [Route("Register/{id}")]
+        public IActionResult Register(int? id)
         {
-            return View();
+
+            var account = new AccountModel();
+
+            if (id != null)
+            {
+                var accountDomain = _myFinanceDbContext.Account.Where(x => x.Id == id).FirstOrDefault();
+
+                account.Id = accountDomain.Id;
+                account.Description = accountDomain.Description;
+                account.Type = accountDomain.Type;
+            }
+
+            return View(account);
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        [Route("Register/{id}")]
+        public IActionResult Register(AccountModel input)
+        {
+            var account = new Account()
+            {
+                Id = input.Id,
+                Description = input.Description,
+                Type = input.Type,
+            };
+
+            if (account.Id == null)
+            {
+                _myFinanceDbContext.Account.Add(account);
+            }
+            else
+            {
+                _myFinanceDbContext.Account.Attach(account);
+                _myFinanceDbContext.Entry(account).State = EntityState.Modified;
+            }
+
+            _myFinanceDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
