7829e1f0cdf6339758df46556234ba75ce6b5f39
priscila andreani <priscilaandreani@gmail.com>
2023-07-04 22:03:47 -0300

    feat[domain]: add transaction

---
+++ myfinance-web-netcore/Domain/Transaction.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace myfinance_web_netcore.Domain
+{
+    public class Transaction
+    {
+        public int? Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+        public string History { get; set; }
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+
+}
+
