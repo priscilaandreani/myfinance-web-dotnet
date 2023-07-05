8e087ccf4b163d328cece00a9a722dca96b86a14
priscila andreani <priscilaandreani@gmail.com>
2023-07-04 22:01:01 -0300

    feat: add new db set for transaction

--- myfinance-web-netcore/MyFinanceDbContext.cs
+++ myfinance-web-netcore/MyFinanceDbContext.cs
@@ -6,11 +6,19 @@ namespace myfinance_web_netcore
     public class MyFinanceDbContext : DbContext
     {
         public DbSet<Account> Account { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Server=localhost;Database=myfinance;User Id=SA;Password=Password123@;TrustServerCertificate=True;";
             optionsBuilder.UseSqlServer(connectionString);
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>().ToTable("account_transaction");
+            modelBuilder.Entity<Transaction>().ToTable("account_transaction").Property(x => x.AccountId).HasColumnName("account_id");
+        }
+
     }
+
 }
